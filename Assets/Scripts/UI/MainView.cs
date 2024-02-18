45401767333c981bc981e98813f31836afa0f17d
UpUpDraw <2427959808@qq.com>
2024-02-18 19:22:34 +0800

    深拷贝工具，新增Newtonsoft,json库，新增tmp中文字体

--- Assets/Scripts/UI/MainView.cs
+++ Assets/Scripts/UI/MainView.cs
@@ -3,33 +3,30 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace UI
+public class MainView: MonoBehaviour
 {
-    public class MainView: MonoBehaviour
+   
+    #region ui component
+    [SerializeField] private Button   btn_add;
+    [SerializeField] private TMP_Text text_count;
+
+    private void Reset()
     {
-       
-        #region ui component
-        [SerializeField] private Button   btn_add;
-        [SerializeField] private TMP_Text text_count;
+        btn_add    = transform.Find("btn_add").GetComponent<Button>();
+        text_count = transform.Find("text_count").GetComponent<TMP_Text>();
+    }
 
-        private void Reset()
+    #endregion
+
+    private int      count = 0;
+    private void Awake()
+    {
+        count = 0;
+        text_count.text = count.ToString();
+        btn_add.onClick.AddListener((() =>
         {
-            btn_add    = transform.Find("btn_add").GetComponent<Button>();
-            text_count = transform.Find("text_count").GetComponent<TMP_Text>();
-        }
-
-        #endregion
-
-        private int      count = 0;
-        private void Awake()
-        {
-            count = 0;
+            count++;
             text_count.text = count.ToString();
-            btn_add.onClick.AddListener((() =>
-            {
-                count++;
-                text_count.text = count.ToString();
-            }));
-        }
+        }));
     }
 }
